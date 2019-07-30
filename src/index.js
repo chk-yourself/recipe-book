52dbe0c7257b26e718b2f528a112465fec4545f3
Christina Kim <christinahyunakim@gmail.com>
2019-07-30 09:49:49 -0500

    Deploy to gh-pages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Amplify from 'aws-amplify';
+import { ApolloProvider } from 'react-apollo';
+import AWSAppSyncClient from 'aws-appsync';
+import { Rehydrated } from 'aws-appsync-react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import config from './aws-exports';
 import './index.css';
-import App from './App';
+import { App } from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+Amplify.configure(config);
+
+const client = new AWSAppSyncClient({
+  url: config.aws_appsync_graphqlEndpoint,
+  region: config.aws_appsync_region,
+  auth: {
+    type: config.aws_appsync_apiKey,
+    apiKey: config.aws_appsync_authenticationType
+  }
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Rehydrated>
+        <App />
+      </Rehydrated>
+    </Router>
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
