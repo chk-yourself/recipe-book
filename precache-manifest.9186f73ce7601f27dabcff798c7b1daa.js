eabee4559c82d82331d2e2da0d74b509488dac9d
Christina Kim <christinahyunakim@gmail.com>
2019-07-30 09:35:13 -0500

    Updates

---
+++ precache-manifest.9186f73ce7601f27dabcff798c7b1daa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98cda56846396788c12ea6e0e7547d5b",
+    "url": "/recipe-book/index.html"
+  },
+  {
+    "revision": "44a14cfc170aea694e51",
+    "url": "/recipe-book/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "499301aa1ebde96176b2",
+    "url": "/recipe-book/static/js/2.73a83311.chunk.js"
+  },
+  {
+    "revision": "44a14cfc170aea694e51",
+    "url": "/recipe-book/static/js/main.2bf13991.chunk.js"
+  },
+  {
+    "revision": "3b717f7a139da6ad6739",
+    "url": "/recipe-book/static/js/runtime~main.5d8d1fe1.js"
+  }
+]);
